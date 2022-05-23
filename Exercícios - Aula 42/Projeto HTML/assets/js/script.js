ae816b61d7b0991562985368a93f385a70abc830
RafaelBalbino <rafaelballabi@hotmail.com>
2022-05-23 14:02:40 -0300

    Atualizado em 23.05.2022 | Adicionando Javascript

--- Exercícios - Aula 42/Projeto HTML/assets/js/script.js
+++ Exercícios - Aula 42/Projeto HTML/assets/js/script.js
@@ -18,6 +18,9 @@ var nomeOk = false
 var email = window.document.querySelector("#email")
 var emailOk = false
 
+var assunto = window.document.querySelector("#assunto")
+var assuntoOk = false
+
 /* Primeira validação fofolete que é se colocou o nome certo ou não. */
 function validarNome() {
     let txtNome = document.querySelector("#txtNome")
@@ -28,10 +31,14 @@ function validarNome() {
         txtNome.style.color = 'red'
         nomeOk = false
     }
-    else{
+    else if(nome.value.length >= 2){
         txtNome.innerHTML = "Belo nome!"
         txtNome.style.color = 'green'
         nomeOk = true
+    }
+    else if(nome.value.length == 0){
+        txtNome.innerHTML = ""
+        nomeOk = false
     }
 }
 
@@ -39,32 +46,59 @@ function validarNome() {
 function validarEmail() {
     let txtEmail = document.querySelector("#txtEmail")
 
-    if(email.value.length < 7) {
+    if(email.value.length < 8) {
         txtEmail.innerHTML = "E-mail inválido!"
         txtEmail.style.color = 'red'
         emailOk = false
     }
-    else {
+    else if(email.value.length >= 8){
         txtEmail.innerHTML = "E-mail fofolete!"
         txtEmail.style.color = 'green'
         emailOk = true
     }
+    else if (email.value.length == 0){
+        txtEmail.innerHTML = ""
+        emailOk = false
+    }
 }
 
-/* Validando a função acima pra mandar um alerta se não houver nome ou for inválido */
+function validarAssunto() {
+    let txtAssunto = document.querySelector("#assunto")
+
+    if(assunto.value.length <= 4) {
+        assuntoOk = false
+    }
+    else {
+        assuntoOk = true
+    }
+}
+
+/* Validando a função acima pra mandar um alerta se não houver nome, assunto 
+ * ou e-mail ou se um deles for inválido. */
 function enviar() {
-    if(nomeOk == true && emailOk == true) {
+    if(nomeOk == true && emailOk == true && assuntoOk == true) {
         alert("CAIU NO CONTO!")
         window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
     }
-    else if(nomeOk == false && emailOk == true){
+    else if(nomeOk == false && emailOk == true && assuntoOk == true){
         alert("Mensagem não enviada! Preencha o seu nome corretamente antes!")
     }
-    else if(nomeOk == true && emailOk == false) {
+    else if(nomeOk == true && emailOk == false && assuntoOk == true) {
         alert("Mensagem não enviada! Preencha o seu e-mail corretamente antes!")
     }
-    else {
+    else if(nomeOk == true && emailOk == true && assuntoOk == false) {
+        alert("Mensagem não enviada! Detalha esse assunto melhor antes!")
+    }
+    else if(nomeOk == false && emailOk == false && assuntoOk == true) {
         alert("Mensagem não enviada! Preencha o seu nome e e-mail corretamente antes!")
     }
+    else if(nomeOk == true && emailOk == false && assuntoOk == false) {
+        alert("Mensagem não enviada! Preencha o seu e-mail corretamente e detalha esse assunto antes!")
+    }
+    else if(nomeOk == false && emailOk == true && assuntoOk == false) {
+        alert("Mensagem não enviada! Preencha o seu nome corretamente e detalha esse assunto antes!")
+    }
+    else {
+        alert("Mensagem não enviada! Preencha seu e-mail e nome corretamente e detalha esse assunto aí!")
+    }
 }
-
